0655c5d36b0cb8a2d411550d7fc0f2fa43f32dcc
konstantin.retunsky@gmail.com <konstantin.retunsky@gmail.com>
2024-11-19 22:15:21 +0500

    update versions

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,100 +1,105 @@
 import org.jetbrains.compose.ExperimentalComposeLibrary
+import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSetTree
 
 plugins {
-    alias(libs.plugins.multiplatform)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.compose)
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlinx.serialization)
+	alias(libs.plugins.multiplatform)
+	alias(libs.plugins.compose.compiler)
+	alias(libs.plugins.compose)
+	alias(libs.plugins.android.application)
+	alias(libs.plugins.kotlinx.serialization)
 }
 
 kotlin {
-    jvmToolchain(11)
-    androidTarget {
-        //https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-test.html
-        instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
-    }
-
-    wasmJs {
-        browser()
-        binaries.executable()
-    }
-
-    listOf(
-        iosX64(),
-        iosArm64(),
-        iosSimulatorArm64()
-    ).forEach {
-        it.binaries.framework {
-            baseName = "ComposeApp"
-            isStatic = true
-        }
-    }
-
-    sourceSets {
-        commonMain.dependencies {
-            implementation(compose.runtime)
-            implementation(compose.foundation)
-            implementation(compose.material3)
-            implementation(compose.components.resources)
-            implementation(compose.components.uiToolingPreview)
-            implementation(libs.kotlinx.coroutines.core)
-            implementation(libs.ktor.client.core)
-            implementation(libs.ktor.client.content.negotiation)
-            implementation(libs.ktor.client.serialization)
-            implementation(libs.ktor.client.logging)
-            implementation(libs.androidx.lifecycle.viewmodel)
-            implementation(libs.androidx.lifecycle.runtime.compose)
-            implementation(libs.androidx.navigation.composee)
-            implementation(libs.kotlinx.serialization.json)
-            implementation(libs.koin.core)
-            implementation(libs.koin.compose)
-            implementation(libs.coil)
-            implementation(libs.coil.network.ktor)
-            implementation(libs.multiplatformSettings)
-            implementation(libs.kotlinx.datetime)
-        }
-
-        commonTest.dependencies {
-            implementation(kotlin("test"))
-            @OptIn(ExperimentalComposeLibrary::class)
-            implementation(compose.uiTest)
-            implementation(libs.kotlinx.coroutines.test)
-        }
-
-        androidMain.dependencies {
-            implementation(compose.uiTooling)
-            implementation(libs.androidx.activityCompose)
-            implementation(libs.kotlinx.coroutines.android)
-            implementation(libs.ktor.client.okhttp)
-        }
-
-        iosMain.dependencies {
-            implementation(libs.ktor.client.darwin)
-        }
-
-    }
+	jvmToolchain(jdkVersion = JavaVersion.VERSION_17.toString().toInt())
+	
+	androidTarget {
+		//https://www.jetbrains.com/help/kotlin-multiplatform-dev/compose-test.html
+		@OptIn(ExperimentalKotlinGradlePluginApi::class)
+		instrumentedTestVariant.sourceSetTree.set(KotlinSourceSetTree.test)
+	}
+	
+	@OptIn(ExperimentalWasmDsl::class)
+	wasmJs {
+		browser()
+		binaries.executable()
+	}
+	
+	listOf(
+		iosX64(),
+		iosArm64(),
+		iosSimulatorArm64()
+	).forEach {
+		it.binaries.framework {
+			baseName = "ComposeApp"
+			isStatic = true
+		}
+	}
+	
+	sourceSets {
+		commonMain.dependencies {
+			implementation(compose.runtime)
+			implementation(compose.foundation)
+			implementation(compose.material3)
+			implementation(compose.components.resources)
+			implementation(compose.components.uiToolingPreview)
+			implementation(libs.kotlinx.coroutines.core)
+			implementation(libs.ktor.client.core)
+			implementation(libs.ktor.client.content.negotiation)
+			implementation(libs.ktor.client.serialization)
+			implementation(libs.ktor.client.logging)
+			implementation(libs.androidx.lifecycle.viewmodel)
+			implementation(libs.androidx.lifecycle.runtime.compose)
+			implementation(libs.androidx.navigation.composee)
+			implementation(libs.kotlinx.serialization.json)
+			implementation(libs.koin.core)
+			implementation(libs.koin.compose)
+			implementation(libs.coil)
+			implementation(libs.coil.network.ktor)
+			implementation(libs.multiplatform.settings)
+			implementation(libs.kotlinx.datetime)
+		}
+		
+		commonTest.dependencies {
+			implementation(kotlin("test"))
+			@OptIn(ExperimentalComposeLibrary::class)
+			implementation(compose.uiTest)
+			implementation(libs.kotlinx.coroutines.test)
+		}
+		
+		androidMain.dependencies {
+			implementation(compose.uiTooling)
+			implementation(libs.androidx.activity.compose)
+			implementation(libs.kotlinx.coroutines.android)
+			implementation(libs.ktor.client.okhttp)
+		}
+		
+		iosMain.dependencies {
+			implementation(libs.ktor.client.darwin)
+		}
+		
+	}
 }
 
 android {
-    namespace = "com.courses"
-    compileSdk = 35
-
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 35
-
-        applicationId = "com.courses.androidApp"
-        versionCode = 1
-        versionName = "1.0.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
+	namespace = "com.courses"
+	compileSdk = 35
+	
+	defaultConfig {
+		minSdk = 21
+		targetSdk = 35
+		
+		applicationId = "com.courses.androidApp"
+		versionCode = 1
+		versionName = "1.0.0"
+		
+		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+	}
 }
 
 //https://developer.android.com/develop/ui/compose/testing#setup
 dependencies {
-    androidTestImplementation(libs.androidx.uitest.junit4)
-    debugImplementation(libs.androidx.uitest.testManifest)
+	androidTestImplementation(libs.androidx.uitest.junit4)
+	debugImplementation(libs.androidx.uitest.test.manifest)
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,32 +1,45 @@
 rootProject.name = "courses"
 
 pluginManagement {
-    repositories {
-        google {
-            content { 
-              	includeGroupByRegex("com\\.android.*")
-              	includeGroupByRegex("com\\.google.*")
-              	includeGroupByRegex("androidx.*")
-              	includeGroupByRegex("android.*")
-            }
-        }
-        gradlePluginPortal()
-        mavenCentral()
-    }
+	includeBuild("build-logic")
+	
+	repositories {
+		google {
+			content {
+				includeGroupByRegex("com.android.*")
+				includeGroupByRegex("com.google.*")
+				includeGroupByRegex("androidx.*")
+				includeGroupByRegex("android.*")
+			}
+		}
+		gradlePluginPortal()
+		mavenCentral()
+	}
 }
 
 dependencyResolutionManagement {
-    repositories {
-        google {
-            content { 
-              	includeGroupByRegex("com\\.android.*")
-              	includeGroupByRegex("com\\.google.*")
-              	includeGroupByRegex("androidx.*")
-              	includeGroupByRegex("android.*")
-            }
-        }
-        mavenCentral()
-    }
+	repositories {
+		google {
+			content {
+				includeGroupByRegex("com.android.*")
+				includeGroupByRegex("com.google.*")
+				includeGroupByRegex("androidx.*")
+				includeGroupByRegex("android.*")
+			}
+		}
+		mavenCentral()
+	}
 }
+
+
 include(":composeApp")
+
+include(":core:network")
+include(":core:database")
+include(":core:datastore")
+include(":core:designsystem")
+
+include(":features:auth")
+include(":features:onboarding")
+
 
